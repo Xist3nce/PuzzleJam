8a7a6eabd0d1ae07fb1d8f1fca696c600451d1db
Trent <Xmedia101@yahoo.com>
2019-06-06 01:34:23 -0400

    Mouse Follow Indicator Added
    Unfinished

---
+++ PuzzleJamOG/Assets/Scripts/Player/MouseHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y,0) ;
+    }
+}
